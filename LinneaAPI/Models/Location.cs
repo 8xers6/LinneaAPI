defa34376e646dd5ceb102de8c46fb6b3449e6ff
8xers6 <109412652+8xers6@users.noreply.github.com>
2024-09-22 14:24:48 +0800

    Update Location.cs
    validation

--- LinneaAPI/Models/Location.cs
+++ LinneaAPI/Models/Location.cs
@@ -3,8 +3,11 @@
     public class Location
     {
         public int Id { get; set; }
+        [Required]
         public string VehicleId { get; set; }
+        [Required]
         public float Latitude { get; set; }
+        [Required]
         public float Longitude { get; set; }
     }
 }
